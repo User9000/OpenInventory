5e1f2c0c366e429a43b9db8de28f41cd4455c699
Carlo Lam <calam12@gmail.com>
2021-01-10 15:52:55 -0800

    updated view for authorization

--- openinventory/accounts/api/views.py
+++ openinventory/accounts/api/views.py
@@ -8,6 +8,9 @@ from rest_framework import permissions
 
 from django.contrib.auth import authenticate, get_user_model
 
+
+from django.db.models import Q
+
 #Creating a new token manually
 from rest_framework_jwt.settings import api_settings
 from .utils import jwt_response_payload_handler
@@ -16,13 +19,16 @@ jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
+
+User = get_user_model()
+
 class AuthView(APIView):
     authentication_classes=[]
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
 
-        print(request.user)
+
         if request.user.is_authenticated:
             return Response({'detail': 'You are already authenticated'}, status=400)
 
@@ -30,8 +36,18 @@ class AuthView(APIView):
         username = data.get('username')
         password = data.get('password')
         user = authenticate(username=username, password=password)
-        #print(user)        
-        payload = jwt_payload_handler(user)
-        token = jwt_encode_handler(payload)
-        response = jwt_response_payload_handler(token, user, request=request)
-        return Response(response)
+
+        qs = User.objects.filter(
+            Q(username__iexact=username) |  Q(email__iexact=username)
+        ).distinct()
+
+        if qs.count() ==1:
+            user_obj = qs.first()
+            if user_obj.check_password(password):
+                user = user_obj
+     
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+                response = jwt_response_payload_handler(token, user, request=request)
+                return Response(response)
+        return Response({'detail': 'Invalid credentials'}, status=401)
