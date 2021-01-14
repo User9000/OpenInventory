4f8de110f0996eac19f8edf791409b714b3d6627
Carlo Lam <calam12@gmail.com>
2021-01-13 22:46:25 -0800

    added pagination class

--- openinventory/accounts/api/user/urls.py
+++ openinventory/accounts/api/user/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token #accounts app
 
-from .views import  UserDetailApiView
+from .views import  UserDetailApiView, UserStatusApiView
 
 app_name ='user-api'
 urlpatterns = [
 
     re_path(r'^(?P<username>\w+)/$',UserDetailApiView.as_view(), name='detail'),
+    re_path(r'^(?P<username>\w+)/status/$',UserStatusApiView.as_view(), name='status'),
+
 
 
 
--- openinventory/accounts/api/user/views.py
+++ openinventory/accounts/api/user/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, pagination
 
 from django.contrib.auth import authenticate, get_user_model
 
@@ -21,8 +21,8 @@ from rest_framework_jwt.settings import api_settings
 
 from .serializers import UserDetailSerializer
 
-
-
+from status.api.serializers import StatusInlineSerializer
+from status.models import Status
 User = get_user_model()
 
 
@@ -32,4 +32,27 @@ class UserDetailApiView(generics.RetrieveAPIView):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserDetailSerializer
     lookup_field = 'username'
+
+
+class UserStatusPagination(pagination.PageNumberPagination):
+    page_size= 4
+
+    
+
+
+
+
+class UserStatusApiView(generics.ListAPIView):
+    #permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+   
+    serializer_class = StatusInlineSerializer
+    pagination_class= UserStatusPagination
+    
+
+    def get_queryset(self, *args, **kwargs):
+        username = self.kwargs.get("username", None)
+
+        if username is None:
+            return Status.objects.none()
+        return  Status.objects.filter(user__username=username)
 
